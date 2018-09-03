6d575631b02fb77ee8cc2a183efa691d5887ce6f
Jack Shen <jack.shen97@gmail.com>
2018-09-03 16:56:38 -0400

    Setting up bird redirect from map, picture bug atm.

--- front/mapGoogle.js
+++ front/mapGoogle.js
@@ -31,7 +31,8 @@ function getLatLong(position) {
             console.log( 'length: ' + msg.length );
             console.log( JSON.stringify( msg ));
             var html_str = "";
-			for (i = 0; i < msg.length; i++) {
+            // var image = 'http://localhost:8080/front/assets/icons/logo.png';
+			for (i = 0; i < 50; i++) {
 				html_str += `
 				<div class="resultItem whiteBkg">
 		            <div class="birdImg" name="` + msg[i].comName + `" onclick="getBird(this.attributes)"></div>
@@ -40,18 +41,51 @@ function getLatLong(position) {
 		                <button name="` + msg[i].comName + `" onclick="toAddList(this.attributes)"><img src="assets/icons/add.svg" alt=""></button>
 		                <button name="` + msg[i].comName + `" onclick="toCheckList(this.attributes)"><img src="assets/icons/check.svg" alt=""></button>
 		            </div>
-	       	 	</div>`
+                    </div>`;
+                
+                //adds a map marker for the bird
+                var latlng = { lat: msg[i].lat, lng: msg[i].lng };
+                var marker = new google.maps.Marker({
+                    position: latlng,
+                    map: map,
+                    title: msg[i].comName,
+                });
 			}
 			document.getElementsByClassName("resultDisp")[0].innerHTML = html_str;
 			// for (i = 0; i < msg.length; i++) {
 			// 	document.getElementsByClassName("birdImg")[i].style.backgroundImage = "url('" + msg[i].birdPic + "')";
-			// }
+            // }
+            
         },
         error: function( xhr, ajaxOptions, thrownError ) {
             alert( "Error: " + xhr.status );
         }
     });
 
+}
+
+//Handle bird item onclick and redirect to bird description page
+function getBird(attributes) {
+	//Create URL to localhost list page
+    var URL = "http://localhost:8080/bird?name=" + attributes.name.value;
+    console.log( "Calling: " + URL );
+
+	//Construct AJAX request to localhost
+	$.ajax({
+		type: "GET",
+		url: URL,
+		data: "{}",
+		dataType: "html",
+		success: function(msg){
+            alert("No error connecting to localhost!");
+            alert(URL);
+		},
+		error: function(xhr, ajaxOptions, thrownError){
+			alert("Error connecting to localhost!" + attributes.name.value);
+			alert(URL);
+		}
+	});
+	window.location.href = URL;
 }
 
 function myMap() {
